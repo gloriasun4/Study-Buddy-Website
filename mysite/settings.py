212c163887196e9e9c0ed39b61469e24c30a3d43
Jacqueline Chao <48360711+JacquelineChao@users.noreply.github.com>
2022-09-24 09:58:37 -0400

    resolve unspecified STATIC_ROOT error in deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+# Added import os here for the django-heroku fix at the bottom
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,8 +118,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+# Added the STATIC_ROOT variable here to fix an error w/ not setting the STATIC_ROOT setting
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Activate django-heroku
+# Use this code to avoid the psycopg2 / django-heroku error
+try:
+    if "HEROKU" in os.environ:
+        import django_heroku
+        django_heroku.settings(locals())
+except ImportError:
+    found = False
