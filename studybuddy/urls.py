ee35575d6c4a61c551c8578c6697f371ef5ecb8d
efg2kgw <efg2kgw@virginia.edu>
2022-10-18 14:59:59 -0400

    adding account url, view - generic

--- studybuddy/urls.py
+++ studybuddy/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'studybuddy'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('account/', views.account, name='account'),
 ]
--- studybuddy/views.py
+++ studybuddy/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import User
 
 def index(request):
     return HttpResponse("Welcome to the Study Buddy App!")
@@ -8,4 +9,7 @@ def index(request):
 
 
 
+def account(request):
+
+    return HttpResponse("Hey welcome to account page you are logged in as {{ user.username }}" )
 
