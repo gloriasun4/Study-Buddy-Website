61cfe560545cf7a549c959d095af00c7a2ac6fb6
syy6zn <syang2468@yahoo.com>
2022-11-10 21:46:22 -0500

    fix websocket for https

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
-import os
-# import django #maybe delete
+import os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django.setup() #maybe delete
 
 from django.core.asgi import get_asgi_application
 
@@ -17,26 +18,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 import studybuddy.routing
 
-from django.urls import re_path
-
-from studybuddy import consumers
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-# django.setup() #maybe delete
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # "websocket": AuthMiddlewareStack(
-    #     URLRouter(
-    #         studybuddy.routing.websocket_urlpatterns
-    #     )
-    # )
-
     "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(
                 URLRouter(
                     studybuddy.routing.websocket_urlpatterns
-                    # [re_path(r'ws/studybuddy/chat/rooms/(?P<room_name>\w+)$/', consumers.ChatConsumer.as_asgi())]
                 )
             )
         )
